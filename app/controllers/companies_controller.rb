773246d43f4572ed37b750bc96459c9eb4ea4648
--global <Nestor.Soto79@gmail.com>
2014-12-18 20:48:31 +0000

    cleaned up some code for date/time adn added buttons

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,40 +1,63 @@
 class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
 
- 
-
+  # GET /companies
+  # GET /companies.json
   def index
     @companies = Company.all
-  
+    
     respond_to do |format|
       format.html
       format.csv { render text: @companies.to_csv }
     end
   end
 
+  # GET /companies/1
+  # GET /companies/1.json
   def show
-    respond_with(@company)
   end
 
+  # GET /companies/new
   def new
     @company = Company.new
-    respond_with(@company)
   end
 
+  # GET /companies/1/edit
   def edit
   end
 
+  # POST /companies
+  # POST /companies.json
   def create
     @company = Company.new(company_params)
-    @company.save
-    respond_with(@company)
+
+    respond_to do |format|
+      if @company.save
+        format.html { redirect_to @company, notice: 'Company was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @company }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /companies/1
+  # PATCH/PUT /companies/1.json
   def update
-    @company.update(company_params)
-    respond_with(@company)
+    respond_to do |format|
+      if @company.update(company_params)
+        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /companies/1
+  # DELETE /companies/1.json
   def destroy
     @company.destroy
     respond_to do |format|
@@ -49,11 +72,13 @@ class CompaniesController < ApplicationController
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_company
       @company = Company.find(params[:id])
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
-      params.require(:company).permit(:name, :manager, :status, :terms, :companies )
+      params.require(:company).permit(:name, :manager, :status, :terms)
     end
 end
